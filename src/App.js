419aa180c96c94f2e92b7e5d30b0c197f6feb5b1
zyl-soichiro <48270554+zyl-soichiro@users.noreply.github.com>
2023-02-14 10:45:04 +0900

    step2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaaa
+          Hello from V2
         </a>
       </header>
     </div>
